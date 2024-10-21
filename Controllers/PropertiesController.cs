f514f615701a9bf9144a0d385344e1972061d2aa
Asem Gado <81575109+Asmblxer@users.noreply.github.com>
2024-10-21 12:29:52 +0300

    remove pagination

--- Controllers/PropertiesController.cs
+++ Controllers/PropertiesController.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using RealStats.Data;
 using RealStats.Models;
 using System.Threading.Tasks;
-using X.PagedList;
 
 namespace RealStats.Controllers
 {
@@ -16,28 +15,13 @@ namespace RealStats.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(string searchString, int? page)
+        public async Task<IActionResult> Index()
         {
-            var properties = from p in _context.Properities
-                             select p;
-
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                properties = properties.Where(p => p.Name.Contains(searchString) 
-                                                || p.Description.Contains(searchString)
-                                                || p.City.Contains(searchString)
-                                                || p.Country.Contains(searchString));
-            }
-
-            int pageSize = 9;
-            int pageNumber = (page ?? 1);
-            var pagedProperties = await properties.Include(p => p.Images).Include(p => p.manager).ToListAsync();
-            return View(pagedProperties.ToPagedList(pageNumber, pageSize));
-        }
-
-        public new IActionResult View(object value)
-        {
-            throw new NotImplementedException();
+            var properties = await _context.Properities
+                .Include(p => p.Images)
+                .Include(p => p.manager)
+                .ToListAsync();
+            return View(properties);
         }
 
         public async Task<IActionResult> Details(int? id)
